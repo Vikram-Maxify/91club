c4f183551d7838796107a449ca86e2137b574d4c
anilyadav-netizen <anilyadav@maxifysolution.com>
2026-09-02 13:13:28 +0530

    by anil

--- client/src/pages/main/Main.js
+++ client/src/pages/main/Main.js
@@ -374,7 +374,7 @@ const Main = () => {
       </div>
 
       <div className="container-section mt-6">
-        <div className="bg-body p-3 rounded-lg pb-5 bg-body">
+        <div className="bg-body p-3 rounded-lg pb-3 bg-body">
           <h3 className="heading-h3 text-whites font-medium mb-2 pt-2">
             Service center
           </h3>
@@ -464,7 +464,7 @@ const Main = () => {
         </div>
 
         <button
-          className="border flex  color-l justify-center items-center border-[var(--bg-color-l)] w-[100%] rounded-full p-1 mt-3 text-base"
+          className="border flex  color-l justify-center items-center border-[var(--bg-color-l)] w-[100%] rounded-full p-1 mt-3 text-base mb-3"
           onClick={() => setShowPopup(true)}
         >
           {" "}
